bed71750cb00d256d62657e0868ad2a2be669f4d
Brendan Heywood <brendan.heywood@gmail.com>
2011-10-23 02:55:58 +1100

    got it to compile

---
+++ theCrag.js
@@ -0,0 +1,26 @@
+
+
+var theCrag = new Object();
+
+theCrag.area = function(id){
+
+	return;
+
+	return new Area({
+		id: 12345,
+		name: 'Gara Gorge'
+	});
+
+};
+
+theCrag.Area = function(obj){
+	// make an area
+
+}
+
+theCrag.prototype.done = function(callback){
+};
+
+
+
+theCrag.Area.prototype = theCrag;

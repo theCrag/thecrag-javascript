fa859adc3ff4146268596aa7ef4d54ff4906f68e
Brendan Heywood <brendan.heywood@gmail.com>
2011-10-23 18:05:19 +1100

    got parent deferred working

--- theCrag.js
+++ theCrag.js
@@ -3,6 +3,9 @@
 var theCrag = {
 	area: function(id){
 		return new Area(id);
+	},
+	route: function(id){
+		return new Route(id);
 	}
 };
 
@@ -11,27 +14,58 @@ var theCrag = {
 /*
  an Area object
  */
-var Node = function(){};
+var Node = function(){
+	this.type = 'unknown';
+};
 
+
+/*
+ * Adds a callback for once we have data available
+ */
 Node.prototype.done = function(callback){
+	var node = this;
 	this.promise.then(function(data){
-		this.data = data.data;
-		callback(this.data);
+		node.data = data.data;
+		callback(node);
+	}, function(error){
+		alert('Error: '+error);
 	});
 };
 
-var Area = function(id){
-	this.promise = $.ajax({url: 'http://dev.thecrag.com/area/'+id+'/json?jsonp=', dataType: "jsonp", jsonp: "jsonp"});
-};
-Area.prototype = new Node();
-Area.prototype.parent = function(){
+/*
+ * Return a Parent with a promise for data
+ */
+Node.prototype.parent = function(){
+	var parent = new Area();
+	var node = this;
+	var deferred = $.Deferred();
 	this.promise.then(function(data){
-		var parent = new Area(data.parent);
-		return parent;
+		var parentId = data.parent || data.data.ancestors[data.data.ancestors.length-1].id;
+		var promise = $.ajax({url: 'http://dev.thecrag.com/area/'+parentId+'/json', dataType: "jsonp", jsonp: "jsonp"});
+		promise.done(function(obj){
+			node.data = obj.data;
+			deferred.resolve(node);
+		});
+	}, function(error){
+		deferred.fail();
+		alert('Error: '+error);
 	});
+	parent.promise = deferred.promise();
+	return parent;
 }
 
-var Route = function(){};
+var Area = function(id){
+	this.type = 'area';
+	if (id){
+		this.promise = $.ajax({url: 'http://dev.thecrag.com/area/'+id+'/json', dataType: "jsonp", jsonp: "jsonp"});
+	}
+};
+Area.prototype = new Node();
+
+var Route = function(id){
+	this.type = 'route';
+	this.promise = $.ajax({url: 'http://dev.thecrag.com/route/'+id+'/json', dataType: "jsonp", jsonp: "jsonp"});
+};
 Route.prototype = new Node();
 
 window.theCrag = theCrag;
